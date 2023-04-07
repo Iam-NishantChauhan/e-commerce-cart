e6e70ae133050cceea9a6fd014e522a2291ca875
Nishant Chauhan <nishantpssn720@gmail.com>
2023-04-07 16:33:45 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./shop/shop";
+import { Contact } from "./contact";
+import { Footer } from "./footer";
+import { Cart } from "./cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
+
+function App() {
+  return (
+    <div className="App">
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/contact.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./contact.css";
+
+export const Contact = () => {
+  return <div class="form">
+    <form>
+    <label id="name">Your Name: </label>
+        <input type="username"></input>
+        <br/>
+        <br/>
+        <label id="contactNo">Contact No: </label>
+        <input type="tel"></input>
+        <br/>
+        <br/>
+        <label id="query">Your Query: </label>
+        <input type="textarea"></input>
+        <br/>
+        <br/>
+        <button class="btn" type="submit">Submit</button>
+    </form>
+  </div>;
+};
